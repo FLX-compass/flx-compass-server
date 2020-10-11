35dfcedc459e22e5a5a3c0ef93375fa3af6395f5
Jason Bunnell <jason.bunnell@gmail.com>
2020-10-11 15:55:17 -0400

    Connect to MongoDB database

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDb = require('./config/db');
 
 // Route files
 const attractions = require('./routes/attractions');
 
 // Load envirement files
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDb();
 
 const app = express();
 
@@ -20,4 +24,14 @@ app.use('/api/v2/attractions', attractions);
 
 const PORT = process.env.PORT||5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+   PORT, 
+   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+   console.log(`Error: ${err.message}`);
+   // Close server & exit process
+   server.close(() => process.exit(1));
+});

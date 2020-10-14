6ada666fd5996d59d5547bdb88b1ba0c4b94f1e4
Jason Bunnell <jason.bunnell@gmail.com>
2020-10-13 20:21:10 -0400

    Get All Attractions and Get Single Attraction controller routes

--- controllers/attractions.js
+++ controllers/attractions.js
@@ -3,15 +3,28 @@ const Attraction = require('../models/Attraction');
 // @desc    Get All Attractions
 // @route   GET /api/v2/attractions
 // @access  Public
-exports.getAttractions = (req, res, next) => {
-   res.status(200).json({ success: true, data: "Show all attractions" });
+exports.getAttractions = async (req, res, next) => {
+   try {
+      const attractions = await Attraction.find();
+      res.status(200).json({ success: true, data: attractions });
+   } catch (err) {
+      res.status(400).json({ success: false });
+   }
 }
 
 // @desc    Get Single Attraction
 // @route   GET /api/v2/attractions/:id
 // @access  Public
-exports.getAttraction = (req, res, next) => {
-   res.status(200).json({ success: true, data: `Get attraction ${req.params.id}` });
+exports.getAttraction = async (req, res, next) => {
+   try {
+      const attraction = await Attraction.findById(req.params.id);
+      if(!attraction) {
+         return res.status(400).json({ success: false });
+      }
+      res.status(200).json({ success: true, data: attraction });
+   } catch (err) {
+      res.status(400).json({ success: false });
+   }
 }
 
 // @desc    Create Attraction

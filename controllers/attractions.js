b8f70f70aa5fa164551ce049bda1967589dcf107
Jason Bunnell <jason.bunnell@gmail.com>
2020-10-15 14:09:45 -0400

    added async middleware with asyncHandler function

--- controllers/attractions.js
+++ controllers/attractions.js
@@ -1,54 +1,46 @@
 const ErrorResponse = require('../utils/errorResponse');
 const Attraction = require('../models/Attraction');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get All Attractions
 // @route   GET /api/v2/attractions
 // @access  Public
-exports.getAttractions = async (req, res, next) => {
-   try {
+exports.getAttractions = asyncHandler(async (req, res, next) => {
       const attractions = await Attraction.find();
-      res.status(200).json({ success: true, count: attractions.length, data: attractions });
-   } catch (err) {
-      next(err);
-   }
-}
+      res
+         .status(200)
+         .json({ success: true, count: attractions.length, data: attractions });
+});
 
 // @desc    Get Single Attraction
 // @route   GET /api/v2/attractions/:id
 // @access  Public
-exports.getAttraction = async (req, res, next) => {
-   try {
+exports.getAttraction = asyncHandler(async (req, res, next) => {
       const attraction = await Attraction.findById(req.params.id);
       if(!attraction) {
          return next(new ErrorResponse(`Attraction not found with ID of ${req.params.id}`, 404));
       }
-      res.status(200).json({ success: true, data: attraction });
-   } catch (err) {
-      next(err);
-   }
-}
+      res
+         .status(200)
+         .json({ success: true, data: attraction });
+});
 
 // @desc    Create Attraction
 // @route   POST /api/v2/attractions
 // @access  Private
-exports.createAttraction = async (req, res, next) => {
-   try {
+exports.createAttraction = asyncHandler(async (req, res, next) => {
       const attraction = await Attraction.create(req.body);
 
       res.status(201).json({
          success: true,
          data: attraction
       });
-   } catch (err) {
-      next(err);
-   }
-}
+});
 
 // @desc    Update Attraction
 // @route   PUT /api/v2/attractions/:id
 // @access  Private
-exports.updateAttraction = async (req, res, next) => {
-   try {
+exports.updateAttraction = asyncHandler(async (req, res, next) => {
       const attraction = await Attraction.findByIdAndUpdate(req.params.id, req.body, {
          new: true,
          runValidators: true
@@ -56,23 +48,20 @@ exports.updateAttraction = async (req, res, next) => {
       if(!attraction) {
          return next(new ErrorResponse(`Attraction not found with ID of ${req.params.id}`, 404));
       }
-      res.status(200).json({ success: true, data: attraction });   
-   } catch (err) {
-      next(err);
-   }  
-};
+      res
+         .status(200)
+         .json({ success: true, data: attraction });   
+});
 
 // @desc    Delete Attraction
 // @route   DELETE /api/v2/attractions/:id
 // @access  Private
-exports.deleteAttraction = async (req, res, next) => {
-   try {
+exports.deleteAttraction = asyncHandler(async (req, res, next) => {
       const attraction = await Attraction.findByIdAndDelete(req.params.id);
       if(!attraction) {
          return next(new ErrorResponse(`Attraction not found with ID of ${req.params.id}`, 404));
       }
-      res.status(200).json({ success: true, data: {} });   
-   } catch (err) {
-      next(err);
-   } 
-}
+      res
+         .status(200)
+         .json({ success: true, data: {} });   
+});

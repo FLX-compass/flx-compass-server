6ac462b793ae69b2280e413ec4dc60ab6374923e
Jason Bunnell <jason.bunnell@gmail.com>
2020-10-27 18:59:47 -0400

    6.40 Added get single product, create product controller and routes.

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Product = require('../models/Product');
+const Attraction = require('../models/Attraction');
 
 // @desc    Get All Products
 // @route   GET /api/v2/products
@@ -24,5 +25,46 @@ exports.getProducts = asyncHandler(async (req, res, next) => {
       success: true,
       count: products.length,
       data: products
+   });
+});
+
+// @desc    Get Single Product
+// @route   GET /api/v2/products/:ID
+// @access  Public
+exports.getProduct = asyncHandler(async (req, res, next) => {
+   const product = await Product.findById(req.params.id).populate({
+      path: 'attraction',
+      select: 'name slug'
+   });
+
+   if(!product) {
+      return next(new ErrorResponse(`No course with an ID of ${req.params.id}`), 404);
+   }
+
+   res.status(200).json({
+      success: true,
+      data: product
+   });
+});
+
+// @desc    Create Product
+// @route   POST /api/v2/attractions/:attractionID/products
+// @access  Private
+exports.addProduct = asyncHandler(async (req, res, next) => {
+   req.body.attraction = req.params.attractionId;
+   const attraction = await Attraction.findById(req.params.attractionId);
+
+   if(!attraction) {
+      return next(
+         new ErrorResponse(`No attraction with an ID of ${req.body.attractionId}`, 
+         488)
+      );
+   }
+
+   const product = await Product.create(req.body);
+
+   res.status(200).json({
+      success: true,
+      data: product
    });
 });
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const {
-   getProducts
+   getProducts,
+   getProduct,
+   addProduct
 } = require('../controllers/products');
 
 const router = express.Router({ mergeParams: true });
 
 router
    .route('/')
-   .get(getProducts);
+   .get(getProducts)
+   .post(addProduct);
+
+router
+   .route('/:id')
+   .get(getProduct);
 
 module.exports = router;

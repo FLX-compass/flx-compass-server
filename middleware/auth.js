fb8cc57acf5ab554abb4d288eeca136ebfdad8f3
Jason Bunnell <jason.bunnell@gmail.com>
2020-10-29 09:36:19 -0400

    7.51 updating token in postman

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,14 @@ const asyncHandler = require('./async');
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 
-
 // Protect routes
 exports.protect = asyncHandler(async (req, res, next) => {
    let token;
 
-   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+   if (
+      req.headers.authorization && 
+      req.headers.authorization.startsWith('Bearer')
+   ) {
       token = req.headers.authorization.split(' ')[1];
    }
 
@@ -24,8 +26,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
    try {
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
       console.log(decoded);
+
       req.user = await User.findById(decoded.id);
+      
       next();
    } catch (err) {
       return next(new ErrorResponse('Not authorized to access this resource.', 401));
